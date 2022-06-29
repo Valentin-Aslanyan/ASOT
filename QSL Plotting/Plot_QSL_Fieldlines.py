14aa94199328316ac7c66751be72cf453ee28a25
Valentin Aslanyan <aslanyan.valentin@gmail.com>
2022-06-29 12:04:30 +0100

    Comment and refactor

--- QSL Plotting/Plot_QSL_Fieldlines.py
+++ QSL Plotting/Plot_QSL_Fieldlines.py
@@ -1,16 +1,22 @@
 
+"""
+Display squashing factor from QSL Squasher (not included in this distribution) on 3D sphere
+Optionally display fieldlines, magnetic nulls and and outer translucent sphere showing outside squashing factor
+"""
 
-file_directory="./PFLS/0044295/"
-R_start    =[+1.000,+1.000,+1.000,+1.000,+1.000,+1.000,+1.000,+1.000,+1.000,+1.000,+1.000,+1.000,+1.000,+1.000,+1.000,+1.000,+1.000,+1.000,+1.000,+1.000,+1.000,+1.000,+1.000,+1.000]
-theta_start=[+2.510,+2.028,+2.048,+2.054,+1.982,+1.990,+1.951,+1.961,+2.006,+2.012,+1.891,+1.834,+1.798,+1.740,+1.692,+1.834,+2.021,+2.706,+2.694,+2.657,+2.543,+2.550,+2.513,+2.527]
-phi_start  =[-0.811,+1.135,-0.282,-0.282,-0.015,-0.015,+0.215,+0.216,-0.162,-0.155,+0.546,+0.318,+0.160,-0.031,-0.202,-0.277,-0.311,-0.210,-0.211,+0.059,+0.320,+0.333,+0.727,+0.740]
-
+file_directory="./PFLS/0000000/"
+R_start    =[ +1.000, +1.000, +1.000, +1.000]
+theta_start=[+24.133,+23.503,+22.930,+22.472]
+phi_start  =[ -8.881, -8.938, -8.594, -7.620]
 plot_R3=True
+plot_nulls=True
 
 
 import sys
 sys.path[:0]=['/Change/This/Path']
 from ASOT_Functions_Python import *
+if plot_nulls:
+	import HQVseg
 import matplotlib.pyplot as plt
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
@@ -37,30 +43,23 @@ phi,theta,R,B=parse_QSL_folder(file_directory)
 phi=(phi-180.0)*np.pi/180.0
 theta=(theta+90.0)*np.pi/180.0
 
+if plot_nulls:
+	null_locations=HQVseg.nulls_from_dir(file_directory)
+	null_point_X=null_locations[:,0]*np.sin(null_locations[:,1]/180.0*np.pi)*np.cos(null_locations[:,2]/180.0*np.pi)
+	null_point_Y=-null_locations[:,0]*np.sin(null_locations[:,1]/180.0*np.pi)*np.sin(null_locations[:,2]/180.0*np.pi)
+	null_point_Z=null_locations[:,0]*np.cos(null_locations[:,1]/180.0*np.pi)
 
-#field_line_start=np.array([1.0,1.99,-0.1])
-#field_line_sph=field_line_spherical(field_line_start,R,theta,phi,B,1.0,3.0,step_size=1E-2)
-#field_line_X=field_line_sph[:,0]*np.sin(field_line_sph[:,1])*np.cos(field_line_sph[:,2])
-#field_line_Y=-field_line_sph[:,0]*np.sin(field_line_sph[:,1])*np.sin(field_line_sph[:,2])
-#field_line_Z=field_line_sph[:,0]*np.cos(field_line_sph[:,1])
-
-#plt.figure()
-#print(np.shape(Q_grid1),np.shape(phi_grid1),np.shape(theta_grid1))
-#plt.plot(phi_grid1[:,0])
-#plt.plot(theta_grid1[0,:])
 
 
 fig=plt.figure("",figsize=(10,7))
-color_plot=plt.pcolormesh(phi_grid1,theta_grid1,Q_grid1,cmap='RdBu_r',vmin=-10,vmax=10)
+color_plot=plt.pcolormesh(phi_grid1,theta_grid1,Q_grid1[:-1,:-1],cmap='RdBu_r',vmin=-10,vmax=10)
 
 
-#mlab.figure(bgcolor=(1,0.8,0.55))
 mlab.figure(bgcolor=(1,1,1))
 
 sphere_mesh=mlab.mesh(X_grid1[::-1,:],Y_grid1[::-1,:],Z_grid1[:,:],scalars=Q_grid1[:,:],colormap='RdBu',vmin=-10,vmax=10)
 sphere_mesh.actor.property.backface_culling = True
 sphere_mesh.module_manager.scalar_lut_manager.reverse_lut = True
-#mlab.colorbar(orientation="vertical")
 
 if plot_R3:
 	sphere_mesh3=mlab.mesh(X_grid3[::-1,:],Y_grid3[::-1,:],Z_grid3[:,:],scalars=Q_grid3[:,:],colormap='RdBu',vmin=-10,vmax=10,opacity=0.4)
@@ -69,13 +68,20 @@ if plot_R3:
 
 
 for idx in range(min(len(R_start),len(theta_start),len(phi_start))):
-	field_line_start=np.array([R_start[idx],theta_start[idx],phi_start[idx]])
+	th_temp,ph_temp=change_angular_coords(theta_start[idx],phi_start[idx],from_type='carrington',to_type='flicks')
+	field_line_start=np.array([R_start[idx],th_temp+0.5*np.pi,ph_temp])
 	field_line_sph=field_line_spherical(field_line_start,R,theta,phi,B,1.0,2.9,step_size=1E-2)
 	field_line_X=field_line_sph[:,0]*np.sin(field_line_sph[:,1])*np.cos(field_line_sph[:,2])
 	field_line_Y=-field_line_sph[:,0]*np.sin(field_line_sph[:,1])*np.sin(field_line_sph[:,2])
 	field_line_Z=field_line_sph[:,0]*np.cos(field_line_sph[:,1])
 	mlab.plot3d(field_line_X,field_line_Y,field_line_Z,line_width=0.01,color=(0.22,0.424,0.69),tube_radius=0.004)#color=(0,0,0)
 	plt.plot(field_line_sph[:,2],field_line_sph[:,1],color="black")
+
+
+if plot_nulls:
+	null_point=mlab.points3d(null_point_X,null_point_Y,null_point_Z,scale_factor=0.025,color=(0.0, 1.0, 0.0))
+	null_point.actor.property.backface_culling = True
+	plt.plot(null_locations[:,2]*DEG2RAD,null_locations[:,1]*DEG2RAD,'+',color="#9933FF")
 
 
 mlab.view(azimuth=0, elevation=110, roll=90, distance=4.0)#, focalpoint=None, roll=None, reset_roll=True, figure=None)
